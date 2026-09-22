4ec4f686ad8d1b256b6579ded712492b464bae3b
Joe Widdifield <joewidd94@gmail.com>
2026-09-22 17:55:33 -0400

    set up player file

--- player.py
+++ player.py
@@ -4,3 +4,28 @@ Joe Widdifield,
 Create a coin matching game between 2 players,
 9/22/2026
 """
+from coin import Coin
+
+class Player:
+    def __init__(self, name):
+        self.__name = name
+        self.__wallet = 20
+        self.__coin = Coin()
+
+    def toss_coin(self):
+        self.__coin.toss()
+
+    def get_coin_side(self):
+        return self.__coin.get_sideup()
+
+    def win_coin(self):
+        self.__wallet += 1
+
+    def lose_coin(self):
+        self.__wallet -= 1
+
+    def get_wallet(self):
+        return self.__wallet
+
+    def get_name(self):
+        return self.__name
