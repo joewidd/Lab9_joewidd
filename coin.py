025622a73a8ec6bc98a552a47ff487c6287f3be1
Joe Widdifield <joewidd94@gmail.com>
2026-09-22 17:47:55 -0400

    set up coin file

--- coin.py
+++ coin.py
@@ -4,3 +4,18 @@ Joe Widdifield,
 Create a coin matching game between 2 players,
 9/22/2026
 """
+import random
+
+class Coin:
+    def __init__(self):
+        self.__sideup = ""
+        self.toss()
+
+    def toss(self):
+        if random.randrange(0, 2) == 0:
+            self.__sideup = "Heads"
+        else:
+            self.__sideup = "Tails"
+
+    def get_sideup(self):
+        return self.__sideup
